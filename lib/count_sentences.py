c3de8fdd945cb966991c84ce98cbe030e7d5ad3e
Dov Azogui <dov.azogui@gmail.com>
2024-10-08 17:27:30 -0400

    created the functions

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self , value = ""):
+    self.value = value
+    
+  @property
+  def value(self):
+    return self._value 
+  
+  @value.setter
+  def value(self, val):
+    if type(val) == str:
+      self._value = val
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    if self.value[len(self.value) -1] == '.':
+      return True
+    else:
+      return False
+  
+  def is_question(self):
+    if self.value[len(self.value) -1] == '?':
+      return True
+    else:
+      return False
+    
+  def is_exclamation(self):
+    if self.value[len(self.value) -1] == '!':
+      return True
+    else:
+      return False
+    
+
+  def count_sentences(self):
+    # Replacing multiple sentence ending punctuations with a period
+    sentences = self.value.replace('!', '.').replace('?', '.').split('.')
+    # Filtering out empty strings from the list      
+    sentences = [s.strip() for s in sentences if s.strip()]        
+    return len(sentences)
+    
